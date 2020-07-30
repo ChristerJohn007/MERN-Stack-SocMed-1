1bb987fd0b225a7d5722d143e216d0786e2b596e
Chesa Gabutero <cjbgabutero@gmail.com>
2020-07-31 01:07:18 +0800

    User Registration

---
+++ routes/api/users.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const router = express.Router();
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+const { check, validationResult } = require('express-validator');
+
+const User = require('../../models/User');
+
+router.post(
+    '/',
+    [
+        check('name', 'Name is required').not().isEmpty(),
+        check('email', 'Please enter a valid email').isEmail(),
+        check(
+            'password',
+            'Please enter a password with 6 or more characters'
+        ).isLength({ min: 6 }),
+    ],
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        const { name, email, password } = req.body;
+
+        try {
+            //User existence
+            let user = await User.findOne({ email });
+
+            if (user) {
+                res.status(400).json({
+                    errors: [{ msg: 'User already exists' }],
+                });
+            }
+
+            //User Avatar
+
+            const avatar = gravatar.url(email, {
+                s: '200',
+                r: 'pg',
+                d: 'mm',
+            });
+
+            user = new User({
+                name,
+                email,
+                avatar,
+                password,
+            });
+
+            // Encrypt using bcrypt
+            const salt = await bcrypt.genSalt(10);
+            user.password = await bcrypt.hash(password, salt);
+            await user.save();
+
+            // Return jsonwebtoken
+            res.send('User registered');
+        } catch (err) {
+            console.error(err.message);
+            return res.status(500).send('Server Error');
+        }
+
+        console.log(req.body);
+        res.send('User route');
+    }
+);
+
+module.exports = router;
